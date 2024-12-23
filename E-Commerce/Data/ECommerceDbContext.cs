957f1a98e3f63f1e3757e5a39b7f25a4e80707dd
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-12-23 12:03:54 +0500

    Add project files.

---
+++ E-Commerce/Data/ECommerceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Data
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
---
+++ E-Commerce/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
